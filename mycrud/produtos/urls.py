6922d54710e19e05b3383af82437e9b8072db8e3
Rodrigo de Melo Marcolino <rodrigomelomarcolino@gmail.com>
2019-04-10 19:17:32 -0300

    Ajustes

--- mycrud/produtos/urls.py
+++ mycrud/produtos/urls.py
@@ -3,15 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cadastro-produtos/', views.cadastro_produtos,
-         name='cadastro_produtos'),
     path('lista-produtos/', views.lista_produtos,
          name='lista_produtos'),
     path('adiciona-produto', views.adiciona_produtos,
-         name='adiciona-adiciona_produtos'),
+         name='adiciona_produtos'),
     path('remover/<int:id>', views.remover_produtos, name='remover'),
-    path('atualiza-form/<int:id>', views.atualiza_form,
-         name='atualiza_form'),
     path('atualiza/<int:id>', views.atualiza_produto,
          name='atualiza_produto'),
 ]
--- mycrud/produtos/views.py
+++ mycrud/produtos/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render, redirect
 from .forms import ProdutosForm
 from .models import Produtos
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
     return render(request, 'index.html')
-
-
-def cadastro_produtos(request):
-    return render(request, 'cadastro_produtos.html')
 
 
 def lista_produtos(request):
@@ -18,6 +15,7 @@ def lista_produtos(request):
 
 
 def adiciona_produtos(request):
+
     form = ProdutosForm(request.POST or None)
 
     if form.is_valid():
@@ -25,11 +23,12 @@ def adiciona_produtos(request):
         messages.success(request, 'Produto cadastrado com sucesso!')
         return redirect('lista_produtos')
 
-    return render(request, 'cadastro_produtos.html')
+    return render(request, 'cadastro_produtos.html', {'form': form})
 
 
 def remover_produtos(request, id):
-    produto = Produtos.objects.get(id=id)
+    produto = get_object_or_404(Produtos, pk=id)
+
     produto.delete()
     return redirect('lista_produtos')
 
@@ -38,14 +37,11 @@ def atualiza_produto(request, id):
     produto = Produtos.objects.get(id=id)
     form = ProdutosForm(request.POST or None, instance=produto)
 
-    if form.is_valid:
-        form.save()
-        messages.success(request, 'Produto atualizado com sucesso')
-        return redirect('lista_produtos')
+    if request.method == "POST":
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Produto atualizado com sucesso')
+            return redirect('lista_produtos')
 
-    return render(request, 'atualiza-form.html')
-
-
-def atualiza_form(request, id):
-    produto = Produtos.objects.get(id=id)
-    return render(request, 'atualiza-form.html', {'produto': produto})
+    return render(request, 'atualiza-form.html', {'form': form,
+                                                  'produto': produto})
